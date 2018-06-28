e3e45d7c21f88e9b169daf5e8e93bfe5865936e8
Himanish Setia <hsetia94@gmail.com>
2018-06-28 15:14:19 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var client = 0
+io.on('connection', function(socket){
+    client++;
+    io.emit('broadcast',client)
+
+    socket.on('user',function(msg){
+        console.log("User : "+msg+" connected.")
+        io.emit('join',msg)
+    })
+
+    socket.on('disconnect',function(){
+        client--;
+        io.emit('disconnected',client)
+    })
+
+    socket.on('message',function(msg){
+        console.log("Message : "+msg)
+        io.emit('message',{"message":msg.message,"name":msg.name})
+    })
+});
+
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
